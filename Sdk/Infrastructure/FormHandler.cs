221467bb2273a8fb6fc7430e360b6a7339bd5a8c
Daniel Persson <daniel.persson@atomia.com>
2014-10-16 13:47:50 +0200

    Added generic form handling.

---
+++ Sdk/Infrastructure/FormHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Atomia.OrderPage.Sdk.Infrastructure
+{
+    public interface IInputHandler<TInput, TDataObject>
+    {
+        TDataObject Translate(TInput input, TDataObject dataObject);
+    }
+
+    public class FormHandler<TInput, TDataObject> where TDataObject : new()
+    {
+        private List<IInputHandler<TInput, TDataObject>> inputHandlers = new List<IInputHandler<TInput, TDataObject>>();
+
+        public TDataObject Handle(TInput input)
+        {
+            var dataObject = new TDataObject();
+
+            foreach (var handler in inputHandlers)
+            {
+                dataObject = handler.Translate(input, dataObject);
+            }
+
+            return dataObject;
+        }
+
+        public void RegisterInputHandler(IInputHandler<TInput, TDataObject> inputHandler)
+        {
+            inputHandlers.Add(inputHandler);
+        }
+    }
+}
