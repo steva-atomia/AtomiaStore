bc498918fc2c7d3e6959dbaa7b86c98c02bb201a
Daniel Persson <daniel.persson@atomia.com>
2014-10-16 15:07:05 +0200

    Collect checkout models.

--- Sdk/Core/Models/CheckoutModel.cs
+++ Sdk/Core/Models/CheckoutModel.cs
@@ -9,4 +9,23 @@ namespace Atomia.OrderPage.Sdk.Core.Models
     {
         public virtual ContactInfo MainContact { get; set; }
     }
+
+    public class ContactInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string OrganizationName { get; set; }
+
+        public string Address { get; set; }
+        public string Address2 { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+    }
 }
