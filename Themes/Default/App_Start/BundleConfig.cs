18ec39817ef80436f26a1e0544345f3a6ffeade5
Daniel Persson <daniel.persson@atomia.com>
2014-11-03 18:36:33 +0100

    Added basic bundle configuration and added knockout.js

---
+++ Themes/Default/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Optimization;
+
+namespace Atomia.OrderPage.Themes.Default
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/bundles/shared").Include(
+                        "~/Scripts/knockout-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css/skeleton").Include(
+                "~/Content/css/skeleton/base.css",
+                "~/Content/css/skeleton/layout.css",
+                "~/Content/css/skeleton/skeleton.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+               "~/Content/css/style.css"));
+        }
+    }
+}
--- Themes/Default/DefaultGlobalEventsHandler.cs
+++ Themes/Default/DefaultGlobalEventsHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using Atomia.OrderPage.Core.Infrastructure;
 
@@ -15,6 +16,7 @@ namespace Atomia.OrderPage.Themes.Default
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             Bootstrapper.Initialise();
         }
