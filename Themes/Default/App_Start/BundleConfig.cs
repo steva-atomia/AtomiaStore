8022ef8803e87544c17799b0580bc492657bee4f
Daniel Persson <daniel.persson@atomia.com>
2014-11-04 14:20:13 +0100

    First verison of KO view model for a page.

--- Themes/Default/App_Start/BundleConfig.cs
+++ Themes/Default/App_Start/BundleConfig.cs
@@ -10,9 +10,10 @@ namespace Atomia.OrderPage.Themes.Default
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-
             bundles.Add(new ScriptBundle("~/bundles/shared").Include(
-                        "~/Scripts/knockout-{version}.js"));
+                        "~/Scripts/vendor/jquery-{version}.js",
+                        "~/Scripts/vendor/knockout-{version}.js",
+                        "~/Scripts/pageviewmodel.js"));
 
             bundles.Add(new StyleBundle("~/Content/css/skeleton").Include(
                 "~/Content/css/skeleton/base.css",
---
+++ WebApp/Scripts/domains/index.js
@@ -0,0 +1,17 @@
+﻿PageViewModel.DomainsViewModel = (function (ko) {
+    var domainQueryType = ko.observable('register');
+
+    var isRegisterQuery = ko.pureComputed(function() {
+        return domainQueryType() === 'register';
+    });
+
+    var isTransferQuery = ko.pureComputed(function() {
+        return domainQueryType() === 'transfer';
+    });
+
+    return {
+        domainQueryType: domainQueryType,
+        isRegisterQuery: isRegisterQuery,
+        isTransferQuery: isTransferQuery
+    }
+}(ko));
