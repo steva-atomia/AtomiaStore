c4dee3aa853629cd2c96f4ca1de9deddffdfe8f5
Daniel Persson <daniel.persson@atomia.com>
2014-11-13 18:45:57 +0100

    Added basic chutzpah setup.

---
+++ WebApp/Scripts/tests/test.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="../viewmodels/domainreg.js" />
+
+QUnit.test("basic", function (assert) {
+    ko.applyBindings(Atomia.ViewModel.DomainReg);
+
+    Atomia.ViewModel.DomainReg.results.push("bloop");
+
+    assert.ok(Atomia.ViewModel.DomainReg.hasResults(), "Yay!");
+});
