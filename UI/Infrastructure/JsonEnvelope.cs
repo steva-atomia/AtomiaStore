05ed3f7b24e59ea64037497cb389951f59400218
Daniel Persson <daniel.persson@atomia.com>
2014-12-10 15:05:20 +0100

    Added JsonEnvelope Fail for ModelState.

--- UI/Infrastructure/JsonEnvelope.cs
+++ UI/Infrastructure/JsonEnvelope.cs
@@ -45,6 +45,28 @@ namespace Atomia.Store.UI.Infrastructure
             };
         }
 
+        public static JsonResult Fail(ModelStateDictionary modelState)
+        {
+            var invalidProperties = modelState.Where(kv => kv.Value.Errors.Count > 0);
+            var data = new Dictionary<string, string>();
+
+            foreach(var invalidProperty in invalidProperties)
+            {
+                var message = string.Join(",", invalidProperty.Value.Errors.Select(e => e.ErrorMessage).ToArray());
+                data.Add(invalidProperty.Key, message);
+            }
+
+            return new JsonResult
+            {
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Data = new
+                {
+                    status = "fail",
+                    data = data
+                }
+            };
+        }
+
         /// <summary>
         /// JSend spec: "An error occurred in processing the request, i.e. an exception was thrown"
         /// </summary>
