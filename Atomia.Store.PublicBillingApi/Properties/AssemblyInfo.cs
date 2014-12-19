697b604f6a1bb64fe6934d6d2ca356638bd63835
Daniel Persson <daniel.persson@atomia.com>
2014-12-19 14:22:48 +0100

    Fixed outdated assembly info.

--- Atomia.Store.PublicBillingApi/Properties/AssemblyInfo.cs
+++ Atomia.Store.PublicBillingApi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Atomia.Store.PublicOrderCart")]
+[assembly: AssemblyTitle("Atomia.Store.PublicBillingApi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Atomia.Store.PublicOrderCart")]
+[assembly: AssemblyProduct("Atomia.Store.PublicBillingApi")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
