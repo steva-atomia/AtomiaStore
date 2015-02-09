5ff82ab934b6e99047c464e1e4eb676c9ea4c772
Daniel Persson <daniel.persson@atomia.com>
2015-02-09 10:10:05 +0100

    Drop support for legacy browsers for now (<= IE8).
    Accordingly remove polyfills, and upgrade jQuery to 2.X branch.
    
    Also use nuget dependency management for vendor scripts.

--- Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.utils.js
+++ Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.utils.js
@@ -61,28 +61,6 @@ Atomia.Utils = Atomia.Utils || {};
         return viewModel;
     }
 
-
-    // Polyfill from MDN. Adds Object.create in IE8 and below, and other older browsers like FF < 4.0
-    if (typeof Object.create !== 'function') {
-        Object.create = (function () {
-            var Temp = function () { };
-            return function (prototype) {
-                if (arguments.length > 1) {
-                    throw Error('Second argument not supported');
-                }
-                if (typeof prototype !== 'object') {
-                    throw TypeError('Argument must be an object');
-                }
-                Temp.prototype = prototype;
-                var result = new Temp();
-                Temp.prototype = null;
-                return result;
-            };
-        })();
-    }
-
-
-
     _.extend(exports, {
         request: request,
         publish: publish,
