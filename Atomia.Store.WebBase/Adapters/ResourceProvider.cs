2d98b6605974ed8b6d4d1603944ba40d1da233e7
Daniel Persson <daniel.persson@atomia.com>
2015-03-18 17:53:00 +0100

    Fixed ResourceProvider not using custom theme resources.

--- Atomia.Store.WebBase/Adapters/ResourceProvider.cs
+++ Atomia.Store.WebBase/Adapters/ResourceProvider.cs
@@ -1,13 +1,21 @@
 ﻿using Atomia.Store.Core;
+using System;
 using System.Web.Mvc;
 
 namespace Atomia.Store.WebBase.Adapters
 {
     public sealed class ResourceProvider : IResourceProvider
     {
+        private readonly IThemeNamesProvider themeNamesProvider = DependencyResolver.Current.GetService<IThemeNamesProvider>();
+
         public string GetResource(string resourceName)
         {
-            return LocalizationHelpers.GlobalResource("Common," + resourceName);
+            var themeClass = themeNamesProvider.GetMainThemeName() + "Common,";
+            var resource = LocalizationHelpers.GlobalResource(themeClass + resourceName);
+
+            return String.IsNullOrEmpty(resource)
+                ? LocalizationHelpers.GlobalResource("Common," + resourceName)
+                : resource;
         }
     }
 }
