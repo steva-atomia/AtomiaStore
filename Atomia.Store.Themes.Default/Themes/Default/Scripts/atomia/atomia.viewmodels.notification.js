a282b8b42e3099d4b05b24b7dfcb7965a4813e9d
Daniel Persson <daniel.persson@atomia.com>
2015-04-08 18:48:13 +0200

    Made knockout NotificationModel and bindings more readable.
    * All methods directly on model object instead of prototype.
    * Remove builtin extensibility from model, it should be done externally.
    * Constructor instead of factory for model.
    * Use standard JavaScript casing for objects and properties.
    * Removed reference paths, they were mostly adding noise.
    * Removed unused Flash method

--- Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.notification.js
+++ Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.notification.js
@@ -1,8 +1,4 @@
-﻿/// <reference path="../../../../Scripts/underscore.js" />
-/// <reference path="../../../../Scripts/knockout-3.2.0.debug.js" />
-/// <reference path="atomia.utils.js" />
-
-/* jshint -W079 */
+﻿/* jshint -W079 */
 var Atomia = Atomia || {};
 Atomia.ViewModels = Atomia.ViewModels || {};
 /* jshint +W079 */
@@ -10,83 +6,51 @@ Atomia.ViewModels = Atomia.ViewModels || {};
 (function (exports, _, ko, utils) {
     'use strict';
 
-    var NotificationModelPrototype,
-        CreateNotificationModel,
-        timeoutId;
+    /** Create notification model. */
+    function NotificationModel() {
+        var self = this;
+     
+        self.messageType = ko.observable('');
+        self.title = ko.observable('');
+        self.message = ko.observable('');
+        self.isOpen = ko.observable(false);
 
-    NotificationModelPrototype = {
         /**
          * Open notification.
          * @param {string} title - Notification title
          * @param {string} message - Notification message
          * @param {string} messageType - Notification message type
          */
-        Open: function Open(title, message, messageType) {
-            if (this.IsOpen()) {
-                this.Close();
+        self.open = function open(title, message, messageType) {
+            if (self.isOpen()) {
+                self.close();
             }
 
-            this.Title(title);
-            this.Message(message);
-            this.MessageType(messageType);
-            this.IsOpen(true);
-        },
-
-        /**
-         * Flash notification that closes after a set timeout
-         * @param {string} title - Notification title
-         * @param {string} message - Notification message
-         * @param {string} messageType - Notification message type
-         * @param {number} timeout - Number of ms message stays open, defaults to 10000
-         */
-        Flash: function Flash(title, message, messageType, timeout) {
-
-            if (timeout === undefined) {
-                timeout = 10000;
-            }
-
-            this.Open(title, message, messageType);
-
-            timeoutId = setTimeout(function () {
-                this.Close();
-            }.bind(this), timeout);
-        },
+            self.title(title);
+            self.message(message);
+            self.messageType(messageType);
+            self.isOpen(true);
+        };
 
         /** Close notification */
-        Close: function Close() {
-            this.IsOpen(false);
-            this.Title('');
-            this.Message('');
-            this.MessageType('');
-
-            clearTimeout(timeoutId);
-        },
+        self.close = function close() {
+            self.isOpen(false);
+            self.title('');
+            self.message('');
+            self.messageType('');
+        };
 
         /** Register subscription on ajax errors and notify */
-        NotifyAjaxErrors: function NotifyAjaxErrors(title, message, messageType) {
+        self.notifyAjaxErrors = function notifyAjaxErrors(title, message, messageType) {
             utils.subscribe('request.error', function () {
-                this.Open(title, message, messageType);
-            }.bind(this));
-        }
-    };
-
-
-    /**
-     * Create notification model. 
-     * @param {Object|Function} - Extensions to default notification model
-     */
-    CreateNotificationModel = function CreateNotificationModel(extensions) {
-        return utils.createViewModel(NotificationModelPrototype, {
-            MessageType: ko.observable(''),
-            Title: ko.observable(''),
-            Message: ko.observable(''),
-            IsOpen: ko.observable(false),
-        }, extensions);
-    };
+                self.open(title, message, messageType);
+            });
+        };
+    }
 
 
     _.extend(exports, {
-        CreateNotificationModel: CreateNotificationModel
+        NotificationModel: NotificationModel
     });
 
 })(Atomia.ViewModels, _, ko, Atomia.Utils);
