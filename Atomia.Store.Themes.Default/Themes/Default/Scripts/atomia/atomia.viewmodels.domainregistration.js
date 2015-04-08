35d8b9152d54e15ee793fc830859e0075a25ae61
Daniel Persson <daniel.persson@atomia.com>
2015-04-08 17:57:40 +0200

    Made knockout DomainRegistrationModel and bindings more readable.
    * All methods directly on model object instead of prototype.
    * Use 'with: Atomia.VM.domainRegistration' binding to make other bindings
    * shorter.
    * Remove builtin extensibility from model, it should be done externally.
    * Constructor instead of factory for model.
    * Use standard JavaScript casing for objects and properties.
    * Removed reference paths, they were mostly adding noise.

--- Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.domainregistration.js
+++ Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.domainregistration.js
@@ -1,10 +1,4 @@
-﻿/// <reference path="../../../../Scripts/underscore.js" />
-/// <reference path="../../../../Scripts/knockout-3.2.0.debug.js" />
-/// <reference path="atomia.utils.js" />
-/// <reference path="atomia.api.domains.js" />
-/// <reference path="atomia.viewmodels.cart.js" />
-
-/* jshint -W079 */
+﻿/* jshint -W079 */
 var Atomia = Atomia || {};
 Atomia.ViewModels = Atomia.ViewModels || {};
 /* jshint +W079 */
@@ -12,200 +6,172 @@ Atomia.ViewModels = Atomia.ViewModels || {};
 (function (exports, _, ko, utils, domainsApi, viewModelsApi) {
     'use strict';
 
-    var DomainRegistrationItemPrototype,
-        CreateDomainRegistrationItem,
-        DomainRegistrationModelPrototype,
-        CreateDomainRegistrationModel;
-    
-    DomainRegistrationItemPrototype = {
+    /**
+     * Create domain registration item view model
+     * @param {Object} instance - the object to create a domain registration item from.
+     */
+    function DomainRegistrationItem(instance) {
+        var self = this;
+        var domainParts = instance.DomainName.split('.');
+        var primaryAttr = _.find(instance.CustomAttributes, function (ca) {
+            return ca.Name === 'Premium';
+        });
+
+        self.isPrimary = primaryAttr !== undefined && primaryAttr.Value === 'true';
+        self.uniqueId = _.uniqueId('dmn');
+        self.domainNameSld = domainParts[0];
+        self.domainNameTld = domainParts[1];
+        self.price = instance.PricingVariants[0].Price;
+        self.renewalPeriod = instance.PricingVariants[0].RenewalPeriod
+
+        // TODO: Remove this when change to using all lower case is done.
+        self.Price = self.price;
+
         /** 
          * Checks if domain registration item is equal to other item based on article number and domain name.
          * @param {Object} other - The item to compare to
          * @returns {boolean} whether the items are equal or not.
          */
-        Equals: function Equals(other) {
+        self.Equals = function Equals(other) {
             var otherDomainNameAttr = _.find(other.CustomAttributes, function (ca) {
-                    return ca.Name === 'DomainName';
-                }),
-                otherDomainName = otherDomainNameAttr !== undefined ? otherDomainNameAttr.Value : undefined;
+                return ca.Name === 'DomainName';
+            });
+            var otherDomainName = otherDomainNameAttr !== undefined ? otherDomainNameAttr.Value : undefined;
 
-            return this.ArticleNumber === other.ArticleNumber && this.DomainName === otherDomainName;
-        }
-    };
+            return self.ArticleNumber === other.ArticleNumber && self.DomainName === otherDomainName;
+        };
 
-    /**
-     * Create domain registration item view model
-     * @param {Object|Function} extensions - extensions to the default domain registration item view model
-     * @param {Object} instance - the object to create a domain registration item from.
-     * @returns the created domain registration item.
+        _.extend(self, instance);
+    }
+
+
+    /** 
+     * Create domain registration view model.
+     * @param {Object} cart - instance of cart to add or remove items to.
      */
-    CreateDomainRegistrationItem = function CreateDomainRegistrationItem(extensions, instance) {
-        var domainParts;
+    function DomainRegistrationModel(cart) {
+        var self = this;
 
-        domainParts = instance.DomainName.split('.');
+        self._cart = cart;
 
-        return utils.createViewModel(DomainRegistrationItemPrototype, {
-            UniqueId: _.uniqueId('dmn'),
-            DomainNameSld: domainParts[0],
-            DomainNameTld: domainParts[1],
-            Price: instance.PricingVariants[0].Price,
-            RenewalPeriod: instance.PricingVariants[0].RenewalPeriod
-        }, instance, extensions);
-    };
-    
-    
-    DomainRegistrationModelPrototype = {
-        
-        /**
-         * Submit a domain search query.
-         */
-        Submit: function Submit() {
-            this.IsLoadingResults(true);
-            this.PrimaryResults.removeAll();
-            this.SecondaryResults.removeAll();
-            this.ShowMoreResults(false);
-            this.SubmittedQuery(this.Query());
-            this.NoResults(false);
+        self.submittedQuery = ko.observable();
+        self.query = ko.observable();
+        self.isLoadingResults = ko.observable(false);
+        self.showMoreResults = ko.observable(false);
+        self.primaryResults = ko.observableArray();
+        self.secondaryResults = ko.observableArray();
+        self.noResults = ko.observable(false);
+
+        self.hasResults = ko.pureComputed(function () {
+            return self.primaryResults().length > 0 || self.secondaryResults().length > 0;
+        });
+
+        self.createDomainRegistrationItem = function(instance) {
+            return new DomainRegistrationItem(instance);
+        }
+
+        /** Submit a domain search query. */
+        self.submit = function submit() {
+            self.isLoadingResults(true);
+            self.primaryResults.removeAll();
+            self.secondaryResults.removeAll();
+            self.showMoreResults(false);
+            self.submittedQuery(self.query());
+            self.noResults(false);
             
-            domainsApi.FindDomains(this.Query(), function (data) {
+            domainsApi.FindDomains(self.query(), function (data) {
                 var domainSearchId = data.DomainSearchId;
 
                 if (data.Results.length === 0 && data.FinishSearch) {
-                    this.NoResults(true);
-                    this.IsLoadingResults(false);
+                    self.noResults(true);
+                    self.isLoadingResults(false);
                 }
                 else if (data.FinishSearch) {
-                    this.UpdateResults(data.Result);
+                    self.updateResults(data.Result);
                 }
                 else {
                     domainsApi.CheckStatus(domainSearchId,
                         function (data) {
                             if (data.Results.length === 0 && data.FinishSearch) {
-                                this.NoResults(true);
-                                this.IsLoadingResults(false);
+                                self.noResults(true);
+                                self.isLoadingResults(false);
                             }
                             else {
-                                this.UpdateResults(data.Results);
+                                self.updateResults(data.Results);
                             }
-                        }.bind(this));
+                        });
                 }
-            }.bind(this));
-        },
+            });
+        };
         
         /** Primary TLD search results have finished loading. */
-        PrimaryResultsAreFinished: function () {
-            if (this.PrimaryResults().length === 0) {
+        self.primaryResultsAreFinished = function primaryResultsAreFinished() {
+            if (self.primaryResults().length === 0) {
                 return false;
             }
 
-            return !_.any(this.PrimaryResults(), function(r) { 
-                return r.Status === 'loading'; 
+            return !_.any(self.primaryResults(), function (r) {
+                return r.Status === 'loading';
             });
-        },
+        };
 
         /** 
          * Create domain registration items from primary and secondary TLD results and update view model.
          * @param {Array} results - The domain search results.
          */
-        UpdateResults: function UpdateResults(results) {
+        self.updateResults = function updateResults(results) {
             var primaryResults = [],
                 secondaryResults = [];
 
             _.each(results, function (result) {
-                var item, primaryAttr;
+                var item = viewModelsApi.AddCartItemExtensions(self._cart, self.createDomainRegistrationItem(result));
 
-                item = viewModelsApi.AddCartItemExtensions(this._Cart, this.CreateDomainRegistrationItem(result));
-
-                primaryAttr = _.find(item.CustomAttributes, function (ca) {
-                    return ca.Name === 'Premium';
-                });
-
-                if (primaryAttr !== undefined && primaryAttr.Value === 'true') {
+                if (item.isPrimary) {
                     primaryResults.push(item);
                 }
                 else {
                     secondaryResults.push(item);
                 }
-            }.bind(this));
+            });
 
             // Set all at once to avoid triggering bindings on each push.
-            this.PrimaryResults(primaryResults);
-            this.SecondaryResults(secondaryResults);
+            self.primaryResults(primaryResults);
+            self.secondaryResults(secondaryResults);
 
-            if (this.PrimaryResultsAreFinished()) {
-                this.IsLoadingResults(false);
+            if (self.primaryResultsAreFinished()) {
+                self.isLoadingResults(false);
             }
-        },
+        };
 
         /** Show more results */
-        SetShowMoreResults: function SetShowMoreResults() {
-            this.ShowMoreResults(true);
-        },
+        self.setShowMoreResults = function setShowMoreResults() {
+            self.showMoreResults(true);
+        };
 
         /**
          * Get template name to use for rendering 'item': 'domainregistration-{primary|secondary}-{available|taken|<status>}
          * @param {Object} item - The item to select template name for
          * @returns the template name.
          */
-        GetTemplateName: function GetTemplateName(item) {
-            var primaryAttr = _.find(item.CustomAttributes, function (ca) {
-                    return ca.Name === 'Premium';
-                }),
-                primary = primaryAttr !== undefined && primaryAttr.Value === 'true',
-                displayType = 'domainregistration-';
-
-            displayType += primary ? 'primary-' : 'secondary-';
-
-            if (primary) {
-                displayType += item.Status === 'available' ? 'available' : 'taken';
+        self.getTemplateName = function getTemplateName(item) {
+            
+            if (item.isPrimary && item.Status === 'available') {
+                return 'domainregistration-primary-available';
+            }
+            else if (item.isPrimary) {
+                return 'domainregistration-primary-taken';
             }
             else {
-                displayType += item.Status;
+                return 'domainregistration-secondary-' + item.Status;
             }
-
-            return displayType;
-        },
-
-        /** Check if there are domain search results on the view model. */
-        _HasResults: function _HasResults() {
-            return this.PrimaryResults().length > 0 || this.SecondaryResults().length > 0;
-        }
-    };
-
-    /** 
-     * Create domain registration view model.
-     * @param {Object} cart - instance of cart to add or remove items to.
-     * @param {Object|Function} extensions - Extensions to the default domain registration view model.
-     * @param {Object|Function} itemExtensions - Extensions to the default domain registration item view model.
-     */
-    CreateDomainRegistrationModel = function CreateDomainRegistrationModel(cart, extensions, itemExtensions) {
-        var defaults;
-
-        defaults = function (self) {
-            return {
-                _Cart: cart,
-                CreateDomainRegistrationItem: _.partial(CreateDomainRegistrationItem, itemExtensions || {}),
-
-                SubmittedQuery: ko.observable(),
-                Query: ko.observable(),
-                IsLoadingResults: ko.observable(false),
-                ShowMoreResults: ko.observable(false),
-                PrimaryResults: ko.observableArray(),
-                SecondaryResults: ko.observableArray(),
-                NoResults: ko.observable(false),
-
-                HasResults: ko.pureComputed(self._HasResults, self)
-            };
         };
-
-        return utils.createViewModel(DomainRegistrationModelPrototype, defaults, extensions);
     };
 
 
 
     /* Module exports */
     _.extend(exports, {
-        CreateDomainRegistrationModel: CreateDomainRegistrationModel
+        DomainRegistrationModel: DomainRegistrationModel
     });
 
 })(Atomia.ViewModels, _, ko, Atomia.Utils, Atomia.Api.Domains, Atomia.ViewModels);
