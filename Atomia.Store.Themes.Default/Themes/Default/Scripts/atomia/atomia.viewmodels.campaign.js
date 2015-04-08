f146df80065ad0fd8a21c304ad11919ebdcdb8a1
Daniel Persson <daniel.persson@atomia.com>
2015-04-08 16:33:14 +0200

    Made knockout CampaignCode model and bindings more readable.
    * All methods directly on model object instead of prototype.
    * Use 'with: Atomia.VM.campaignCode' binding to make other bindings shorter.
    * Remove builtin extensibility from model, it should be done externally.
    * Constructor instead of factory for model.
    * Use standard JavaScript casing for objects and properties.
    * Removed reference paths, they were mostly adding noise.

--- Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.campaign.js
+++ Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.campaign.js
@@ -1,8 +1,4 @@
-﻿/// <reference path="../../../../Scripts/underscore.js" />
-/// <reference path="../../../../Scripts/knockout-3.2.0.debug.js" />
-/// <reference path="atomia.utils.js" />
-
-/* jshint -W079 */
+﻿/* jshint -W079 */
 var Atomia = Atomia || {};
 Atomia.ViewModels = Atomia.ViewModels || {};
 /* jshint +W079 */
@@ -10,40 +6,35 @@ Atomia.ViewModels = Atomia.ViewModels || {};
 (function (exports, _, ko, utils) {
     'use strict';
 
-    var CampaignModelPrototype,
-        CreateCampaignModel;
-
-
-    CampaignModelPrototype = {
-        /** Add campaign code to cart. */
-        AddToCart: function AddToCart() {
-            var code = this.Code();
-            if (_.isString(code) && code !== '') {
-                this._Cart.AddCampaignCode(code);
-                this.Added(true);
-                this.Code('');
-            }
-        }
-    };
-
     /** 
      * Knockout view model for adding a campaign code to cart. 
-     * @param {Object} cart - Instance of cart wher campaign code can be added.
-     * @param {Object|Function} extensions - Extensions to the default campaign view model.
+     * @param {Object} cart - Instance of cart where campaign code can be added.
      * @returns the created campaign view model
      */
-    CreateCampaignModel = function CreateCampaignModel(cart, extensions) {
-        return utils.createViewModel(CampaignModelPrototype, {
-            _Cart: cart,
-            Code: ko.observable(''),
-            Added: ko.observable(false)
-        }, extensions);
+    function CampaignCodeModel(cart) {
+        var self = this;
+
+        self._cart = cart;
+
+        self.code = ko.observable('');
+        self.added = ko.observable(false);
+
+        /** Add campaign code to cart. */
+        self.addToCart = function addToCart() {
+            var code = self.code();
+
+            if (_.isString(code) && code !== '') {
+                self._cart.AddCampaignCode(code);
+                self.added(true);
+                self.code('');
+            }
+        };
     };
 
 
     /* Module exports */
     _.extend(exports, {
-        CreateCampaignModel: CreateCampaignModel
+        CampaignCodeModel: CampaignCodeModel
     });
 
 })(Atomia.ViewModels, _, ko, Atomia.Utils);
