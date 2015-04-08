e539504fd10e312fea650072156e9a891825fa90
Daniel Persson <daniel.persson@atomia.com>
2015-04-08 17:09:09 +0200

    Made knockout DomainConnnection and DomainStatus models and bindings more readable.
    * All methods directly on model object instead of prototype.
    * Remove builtin extensibility from model, it should be done externally.
    * Constructor instead of factory for model.
    * Use standard JavaScript casing for objects and properties.
    * Removed reference paths, they were mostly adding noise.

--- Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.domainconnection.js
+++ Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.domainconnection.js
@@ -1,25 +1,28 @@
-﻿/// <reference path="../../../../Scripts/underscore.js" />
-/// <reference path="../../../../Scripts/knockout-3.2.0.debug.js" />
-/// <reference path="atomia.utils.js" />
-
-/* jshint -W079 */
+﻿/* jshint -W079 */
 var Atomia = Atomia || {};
 Atomia.ViewModels = Atomia.ViewModels || {};
 /* jshint +W079 */
 
 (function (exports, _, ko, utils) {
     'use strict';
+    
+    /** Create domain connection view model. 
+     * @param {Object} cart - Instance of cart to operate on.
+     * @param {Object} selectedItem - Item to connect domain to. Can be an observable.
+     */
+    function DomainConnectionModel(cart, selectedItem) {
+        var self = this;
 
-    var DomainConnectionModelPrototype,
-        CreateDomainConnectionModel,
-        CreateDomainStatusModel;
+        self._cart = cart;
 
-
-    DomainConnectionModelPrototype = {
+        self.selectedItem = _.isFunction(selectedItem) ? selectedItem : function () { return selectedItem; };
+        self.uniqueId = _.uniqueId('domain-connection-');
+        self.domainNameOptions = ko.observableArray();
+        self.selectedDomainName = ko.observable();
 
         /** Set domain name if 'SelectedItem' already has domain name associated. */
-        SetInitialDomainName: function () {
-            var selectedItem = this.SelectedItem(),
+        self.setInitialDomainName = function setInitialDomainName() {
+            var selectedItem = self.selectedItem(),
                 itemInCart,
                 domainName;
 
@@ -32,13 +35,13 @@ Atomia.ViewModels = Atomia.ViewModels || {};
             }
 
             if (domainName !== undefined) {
-                this.SelectedDomainName(domainName);
+                self.selectedDomainName(domainName);
             }
-        },
+        };
 
         /** Handler for updating domain name options when cart is updated. */
-        _UpdateDomainNameOptions: function _UpdateDomainNameOptions() {
-            var domainItems = this._Cart.DomainItems(),
+        self.updateDomainNameOptions = function updateDomainNameOptions() {
+            var domainItems = self._cart.DomainItems(),
                 domainNames = [];
 
             if (domainItems !== undefined) {
@@ -48,19 +51,19 @@ Atomia.ViewModels = Atomia.ViewModels || {};
                 });
             }
             
-            this.DomainNameOptions(domainNames);
+            self.domainNameOptions(domainNames);
 
-            if (this.StatusNotifier !== undefined) {
-                this.StatusNotifier.DomainNameOptionsCount(domainNames.length);
+            if (self.statusNotifier !== undefined) {
+                self.statusNotifier.domainNameOptionsCount(domainNames.length);
             }
-        },
+        };
 
         /** Add or remove association between 'selectedDomainName' and 'SelectedItem' in cart. */
-        _HandleSelectedDomainName: function _HandleSelectedDomainName(selectedDomainName) {
-            var selectedItem = this.SelectedItem();
+        self._handleSelectedDomainName = function _handleSelectedDomainName(selectedDomainName) {
+            var selectedItem = self.selectedItem();
 
-            if (this.StatusNotifier !== undefined) {
-                this.StatusNotifier.DomainNameHasBeenSelected(selectedDomainName !== undefined);
+            if (self.statusNotifier !== undefined) {
+                self.statusNotifier.domainNameHasBeenSelected(selectedDomainName !== undefined);
             }
 
             if (selectedItem === undefined || !selectedItem.IsInCart()) {
@@ -68,80 +71,51 @@ Atomia.ViewModels = Atomia.ViewModels || {};
             }
 
             if (selectedDomainName !== undefined) {
-                this._Cart.AddDomainName(selectedItem, selectedDomainName);
+                self._cart.AddDomainName(selectedItem, selectedDomainName);
             }
             else {
-                this._Cart.RemoveDomainName(selectedItem);
+                self._cart.RemoveDomainName(selectedItem);
             }
-        },
+        };
 
         /** Update domain connection when an item ('addedItem') is added to cart. */
-        _HandleAddedCartItem: function _HandleAddedCartItem(addedItem) {
-            var selectedItem = this.SelectedItem();
+        utils.subscribe('cart:add', function(addedItem) {
+            var selectedItem = self.selectedItem();
 
             if (addedItem.IsDomainItem()) {
-                this._UpdateDomainNameOptions();
+                self.updateDomainNameOptions();
             }
             else if (selectedItem.Equals(addedItem)) {
-                this._HandleSelectedDomainName(this.SelectedDomainName());
+                self._handleSelectedDomainName(self.selectedDomainName());
             }
-        },
+        });
 
         /** Update domain connection when an item ('removedItem') is removed from cart. */
-        _HandleRemovedCartItem: function _HandleRemovedCartItem(removedItem) {
+        utils.subscribe('cart:remove', function (removedItem) {
             if (removedItem.IsDomainItem()) {
-                this._UpdateDomainNameOptions();
+                self.updateDomainNameOptions();
             }
-        }
-    };
-    
-    /** Create domain connection view model. 
-     * @param {Object} cart - Instance of cart to operate on.
-     * @param {Object} selectedItem - Item to connect domain to. Can be an observable.
-     * @param {Object|Function} extensions - Extensions to the default view model.
-     */
-    CreateDomainConnectionModel = function CreateDomainConnectionModel(cart, selectedItem, extensions) {
-        var item;
+        });
 
-        item = utils.createViewModel(DomainConnectionModelPrototype, {
-            _Cart: cart,
-            SelectedItem: _.isFunction(selectedItem) ? selectedItem : function () { return selectedItem; },
-            UniqueId: _.uniqueId('domain-connection-'),
-            DomainNameOptions: ko.observableArray(),
-            SelectedDomainName: ko.observable()
-        }, extensions);
-
-        item.SelectedDomainName.subscribe(item._HandleSelectedDomainName);
-        
-        utils.subscribe('cart:add', item._HandleAddedCartItem);
-        utils.subscribe('cart:remove', item._HandleRemovedCartItem);
-
-        item._UpdateDomainNameOptions();
-
-        return item;
-    };
+        self.selectedDomainName.subscribe(self._handleSelectedDomainName);
+    }
 
 
 
-    /** Create and object to relay status of domain connection.
-     * @param {Object|Function} extensions - Extensions to the default domain status view model.
-      */
-    CreateDomainStatusModel = function CreateDomainStatusModel(extensions) {
-        var statusItem = Object.create({}),
-            defaults = {
-                DomainNameHasBeenSelected: ko.observable(false),
-                DomainNameOptionsCount: ko.observable(0)
-            };
+    /** Create and object to relay status of domain connection. */
+    function DomainStatusModel() {
+        var self = this;
 
-        return _.extend(statusItem, defaults, extensions);
-    };
+        self.domainNameHasBeenSelected = ko.observable(false);
+        self.domainNameOptionsCount = ko.observable(0);
+    }
 
 
 
     /* Module exports */
     _.extend(exports, {
-        CreateDomainStatusModel: CreateDomainStatusModel,
-        CreateDomainConnectionModel: CreateDomainConnectionModel
+        DomainStatusModel: DomainStatusModel,
+        DomainConnectionModel: DomainConnectionModel
     });
 
 })(Atomia.ViewModels, _, ko, Atomia.Utils);
