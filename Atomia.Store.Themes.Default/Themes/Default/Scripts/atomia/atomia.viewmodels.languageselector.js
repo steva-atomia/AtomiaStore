ef097af68d20885d397df8d294d2626befabf427
Daniel Persson <daniel.persson@atomia.com>
2015-04-08 18:39:47 +0200

    Made knockout LanguageItem and LanguageSelectorModel and bindings more readable.
    * All methods directly on model objects instead of prototype.
    * Remove builtin extensibility from models, it should be done externally.
    * Constructors instead of factory for model.
    * Use standard JavaScript casing for objects and properties.
    * Removed reference paths, they were mostly adding noise.
    * Cleaned up load method
    * Made attributes available on language items explicit (name, shortName)

--- Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.languageselector.js
+++ Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.languageselector.js
@@ -1,18 +1,10 @@
-﻿/// <reference path="../../../../Scripts/underscore.js" />
-/// <reference path="../../../../Scripts/knockout-3.2.0.debug.js" />
-/// <reference path="atomia.utils.js" />
-
-/* jshint -W079 */
+﻿/* jshint -W079 */
 var Atomia = Atomia || {};
 Atomia.ViewModels = Atomia.ViewModels || {};
 /* jshint +W079 */
 
 (function (exports, _, ko, utils) {
     'use strict';
-
-    var LanguageSelectorModelPrototype,
-        CreateLanguageSelectorModel,
-        CreateLanguageItem;
 
     /**
      * Utility function to convert current URL to a URL with ?lang= query string for switching language
@@ -37,85 +29,71 @@ Atomia.ViewModels = Atomia.ViewModels || {};
 
     /**
      * Create a language item.
-     * @param {Object|Function} extensions - Extensions to the default language item view model.
      * @param {Object} instance - The object to create language item from
      */
-    CreateLanguageItem = function CreateLanguageItem(extensions, instance) {
-        var defaults;
-        
-        defaults = {
-            ChangeLanguageUrl: getNewLanguageURL(instance._CurrentLanguage.Tag, instance.Tag)
-        };
-        
-        return utils.createViewModel({}, defaults, instance, extensions);
-    };
+    function LanguageItem(languageData, currentLanguageData) {
+        var self = this;
 
-    LanguageSelectorModelPrototype = {
+        self.shortName = languageData.ShortName;
+        self.name = languageData.Name;
+        self.changeLanguageUrl = getNewLanguageURL(currentLanguageData.Tag, languageData.Tag);
+    }
+
+    
+    /** Create language selector view model. */
+    function LanguageSelectorModel() {
+        var self = this;
+
+        self.isOpen = ko.observable(false);
+        self.languages = ko.observableArray();
+        self.selectedLanguage = ko.observable();
+
+
+        /** Creates a new LanguageItem object from 'languageData' and 'currentLanguageData' */
+        self.createLanguageItem = function(languageData, currentLanguageData) {
+            return new LanguageItem(languageData, currentLanguageData);
+        };
+
+
         /** Open or close language selector based on current state. */
-        ToggleDropdown: function ToggleDropdown() {
-            if (this.IsOpen()) {
-                this.IsOpen(false);
+        self.toggleDropdown = function toggleDropdown() {
+            if (self.isOpen()) {
+                self.isOpen(false);
             }
             else {
                 utils.publish('dropdown:open');
-                this.IsOpen(true);
+                self.isOpen(true);
             }
-        },
-
-        /** Close language selector. */
-        CloseDropdown: function CloseDropdown() {
-            this.IsOpen(false);
-        },
-
-        /** Load language data generated on server. */
-        Load: function Load(getLanguagesResponse) {
-            this._UpdateLanguages(getLanguagesResponse.data);
-        },
-
-        _UpdateLanguages: function _UpdateLanguages(data) {
-            var tmpLanguages = [],
-                currentLanguage = data.CurrentLanguage;
-
-            this.SelectedLanguage(currentLanguage);
-
-            _.each(data.Languages, function (language) {
-                language._CurrentLanguage = currentLanguage;
-
-                tmpLanguages.push(this.CreateLanguageItem(language));
-            }, this);
-
-            this.Languages(tmpLanguages);
-        }
-    };
-
-
-    /**
-     * Create language selector view model.
-     * @param {Object|Function} extensions - Extensions to the default language selector view model.
-     * @param {Object|Function} itemExtensions - Extensions to the default language item selector
-     */
-    CreateLanguageSelectorModel = function CreateLanguageSelectorModel(extensions, itemExtensions) {
-        var defaults, viewModel;
-
-        defaults = {
-            CreateLanguageItem: _.partial(CreateLanguageItem, itemExtensions || {}),
-            IsOpen: ko.observable(false),
-            Languages: ko.observableArray(),
-            SelectedLanguage: ko.observable()
         };
 
-        viewModel = utils.createViewModel(LanguageSelectorModelPrototype, defaults, extensions);
+        /** Close language selector. */
+        self.closeDropdown = function closeDropdown() {
+            self.isOpen(false);
+        };
+
+        /** Load language data generated on server. */
+        self.load = function load(response) {
+            var tmpLanguages = [];
+            var currentLanguageData = response.data.CurrentLanguage;
+            var currentLanguage = self.createLanguageItem(currentLanguageData, currentLanguageData);
+
+            self.selectedLanguage(currentLanguage);
+
+            _.each(response.data.Languages, function (languageData) {
+                tmpLanguages.push(self.createLanguageItem(languageData, currentLanguageData));
+            });
+
+            self.languages(tmpLanguages);
+        };
 
         utils.subscribe('dropdown:open', function () {
-            viewModel.IsOpen(false);
+            self.isOpen(false);
         });
-
-        return viewModel;
-    };
+    }
 
 
     _.extend(exports, {
-        CreateLanguageSelectorModel: CreateLanguageSelectorModel
+        LanguageSelectorModel: LanguageSelectorModel
     });
 
 })(Atomia.ViewModels, _, ko, Atomia.Utils);
