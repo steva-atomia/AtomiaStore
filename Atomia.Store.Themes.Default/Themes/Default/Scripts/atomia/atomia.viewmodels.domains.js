2953840f971d88b0213a409f5ff8b6237447beef
Daniel Persson <daniel.persson@atomia.com>
2015-04-08 18:04:53 +0200

    Made knockout DomainsModel and bindings more readable.
    * All methods directly on model object instead of prototype.
    * Use 'with: Atomia.VM.domains' binding to make other bindings shorter.
    * Remove builtin extensibility from model, it should be done externally.
    * Constructor instead of factory for model.
    * Use standard JavaScript casing for objects and properties.
    * Removed reference paths, they were mostly adding noise.

--- Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.domains.js
+++ Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.domains.js
@@ -1,8 +1,4 @@
-﻿/// <reference path="../../../../Scripts/underscore.js" />
-/// <reference path="../../../../Scripts/knockout-3.2.0.debug.js" />
-/// <reference path="atomia.utils.js" />
-
-/* jshint -W079 */
+﻿/* jshint -W079 */
 var Atomia = Atomia || {};
 Atomia.ViewModels = Atomia.ViewModels || {};
 /* jshint +W079 */
@@ -10,47 +6,27 @@ Atomia.ViewModels = Atomia.ViewModels || {};
 (function (exports, _, ko, utils) {
     'use strict';
 
-    var DomainsModelPrototype,
-        CreateDomainsModel;
+    /** Create domains view model. */
+    function DomainsModel(extensions) {
+        var self = this;
 
+        self.queryType = ko.observable('domainreg');
+        
+        /** Checks if 'domainreg' is the current 'queryType'. */
+        self.domainRegistrationActive = ko.pureComputed(function() {
+            return self.queryType() === 'domainreg';
+        });
 
-    DomainsModelPrototype = {
-        /** Checks if 'domainreg' is the current 'QueryType'. */
-        _DomainRegistrationActive: function _DomainRegistrationActive() {
-            return this.QueryType() === 'domainreg';
-        },
-
-        /** Checks if 'transfer' is the current 'QueryType'. */
-        _MoveDomainActive: function _MoveDomainActive() {
-            return this.QueryType() === 'transfer';
-        }
-    };
-
-    /**
-     * Create domains view model.
-     * @param {Object|Function} extensions - Extensions to the default domain view model.
-     * @returns the created domains view model.
-     */
-    CreateDomainsModel = function CreateDomainsModel(extensions) {
-        var defaults;
-
-        defaults = function (self) {
-            return {
-                QueryType: ko.observable('domainreg'),
-
-                DomainRegistrationActive: ko.pureComputed(self._DomainRegistrationActive, self),
-                MoveDomainActive: ko.pureComputed(self._MoveDomainActive, self)
-            };
-        };
-
-        return utils.createViewModel(DomainsModelPrototype, defaults, extensions);
-    };
-
+        /** Checks if 'transfer' is the current 'queryType'. */
+        self.moveDomainActive = ko.pureComputed(function () {
+            return self.queryType() === 'transfer';
+        });
+    }
 
 
     /* Module exports */
     _.extend(exports, {
-        CreateDomainsModel: CreateDomainsModel
+        DomainsModel: DomainsModel
     });
 
 })(Atomia.ViewModels, _, ko, Atomia.Utils);
