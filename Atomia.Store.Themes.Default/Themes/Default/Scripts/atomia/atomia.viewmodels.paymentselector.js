ce228370bc667910510031d57dbd71023aaf8eb8
Daniel Persson <daniel.persson@atomia.com>
2015-04-09 09:47:11 +0200

    Made knockout PaymentSelectorModel and PayWithInvoiceModel and bindings more readable.
    * All methods directly on model object instead of prototype.
    * Remove builtin extensibility from model, it should be done externally.
    * Constructor instead of factory for model.
    * Use standard JavaScript casing for objects and properties.
    * Removed reference paths, they were mostly adding noise.
    * Publish 'uiSelectedPaymentMethod' event instead of direct reference between models.

--- Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.paymentselector.js
+++ Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.paymentselector.js
@@ -1,8 +1,4 @@
-﻿/// <reference path="../../../../Scripts/underscore.js" />
-/// <reference path="../../../../Scripts/knockout-3.2.0.debug.js" />
-/// <reference path="atomia.utils.js" />
-
-/* jshint -W079 */
+﻿/* jshint -W079 */
 var Atomia = Atomia || {};
 Atomia.ViewModels = Atomia.ViewModels || {};
 /* jshint +W079 */
@@ -10,21 +6,21 @@ Atomia.ViewModels = Atomia.ViewModels || {};
 (function (exports, _, ko, utils) {
 	'use strict';
 
-	var CreatePaymentSelectorModel;
+    /** Create payment selector */
+	function PaymentSelectorModel() {
+	    var self = this;
 
-    /** Create payment selector
-     * @param {Object|function} extensions - Extensions to the default payment selector model
-     */
-	CreatePaymentSelectorModel = function CreatePaymentSelectorModel(extensions) {
-	    return utils.createViewModel({}, {
-	        SelectedPaymentMethod: ko.observable()
-	    }, extensions);
-	};
+	    self.selectedPaymentMethod = ko.observable();
+
+	    self.selectedPaymentMethod.subscribe(function (paymentMethod) {
+	        utils.publish('uiSelectedPaymentMethod', paymentMethod);
+	    });
+	}
 
 
 	/* Module exports */
 	_.extend(exports, {
-	    CreatePaymentSelectorModel: CreatePaymentSelectorModel
+	    PaymentSelectorModel: PaymentSelectorModel
 	});
 
 })(Atomia.ViewModels, _, ko, Atomia.Utils);
--- Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.paywithinvoice.js
+++ Atomia.Store.Themes.Default/Themes/Default/Scripts/atomia/atomia.viewmodels.paywithinvoice.js
@@ -1,9 +1,4 @@
-﻿/// <reference path="../../../../Scripts/underscore.js" />
-/// <reference path="../../../../Scripts/knockout-3.2.0.debug.js" />
-/// <reference path="atomia.utils.js" />
-/// <reference path="atomia.viewmodels.cart.js" />
-
-/* jshint -W079 */
+﻿/* jshint -W079 */
 var Atomia = Atomia || {};
 Atomia.ViewModels = Atomia.ViewModels || {};
 /* jshint +W079 */
@@ -11,106 +6,88 @@ Atomia.ViewModels = Atomia.ViewModels || {};
 (function (exports, _, ko, utils, viewModelsApi) {
 	'use strict';
 
-	var PayWithInvoiceModelPrototype,
-        CreatePayWithInvoiceModel;
-    
+    /** 
+     * Create pay with invoice model.
+     * @param {Object} cart - A cart view model instance.
+     */
+	function PayWithInvoiceModel(cart) {
+		var self = this;
 
-	PayWithInvoiceModelPrototype = {
+		self._cart = cart;
+		self.invoiceType = ko.observable();
+		self.postalFeeItem = ko.observable(null);
+
+	    /** Make cartable item out of postal fee item.*/
+	    self.createPostalFeeItem = function createPostalFeeItem() {
+	        var cartItem, postalFeeItem = self.postalFeeItem();
+
+	        if (postalFeeItem === null) {
+	            return null;
+	        }
+
+	        postalFeeItem.CustomAttributes.push({
+	            Name: 'NotRemovable',
+	            Value: 'true'
+	        });
+
+	        cartItem = viewModelsApi.AddCartItemExtensions(self._cart, postalFeeItem);
+
+	        return cartItem;
+	    };
+
+	    /** Load postal fee item data generated on server. */
+	    self.loadPostalFeeItem = function loadPostalFeeItem(response) {
+	        var item;
+
+	        if (response.status === 'success') {
+	            item = response.data.Item;
+
+	            // Extend with some fake values that are needed in cart summary before recalc.	
+	            _.extend(item, {
+	                Price: item.PricingVariants[0].Price,
+	                RenewalPeriod: null,
+	                Category: '',
+	                Discount: 0,
+	                Total: item.PricingVariants[0].Price
+	            });
+
+	            self.postalFeeItem(item);
+	        }
+	        else {
+	            self.postalFeeItem(null);
+	        }
+	    }
+
+	    /** Callback handler for when invoice type is selected. Adds or removes postal fee from cart. */
+	    self.invoiceType.subscribe(function (newInvoiceType) {
+	        var cartItem = self.createPostalFeeItem();
+
+	        if (cartItem !== null && newInvoiceType === 'email') {
+	            self._cart.Remove(cartItem);
+	        }
+	        else if (cartItem !== null && newInvoiceType === 'post') {
+	            self._cart.Add(cartItem);
+	        }
+	    });
+		
 	    /** 
          * Callback handler for when payment method is selected. 
 	     * Removes postal fee from cart if payment method is not invoice.
 	     */
-		_SelectPaymentMethod: function _SelectPaymentMethod(newValue) {
-			var cartItem = this._CreatePostalFeeItem();
+	    utils.subscribe('uiSelectedPaymentMethod', function(paymentMethod){
+	        var cartItem = self.createPostalFeeItem();
 
-			if (cartItem !== null && newValue !== 'PayWithInvoice') {
-				this._Cart.Remove(cartItem);
-			}
+	        if (cartItem !== null && paymentMethod !== 'PayWithInvoice') {
+	            self._cart.Remove(cartItem);
+	        }
 
-			this.InvoiceType('email');
-		},
-
-        /** Callback handler for when invoice type is selected. Adds or removes postal fee from cart. */
-		_SelectInvoiceType: function _SelectInvoiceType(newValue) {
-			var cartItem = this._CreatePostalFeeItem();
-
-			if (cartItem !== null && newValue === 'email') {
-				this._Cart.Remove(cartItem);
-			}
-			else if (cartItem !== null && newValue === 'post') {
-				this._Cart.Add(cartItem);
-			}
-		},
-
-        /** Make cartable item out of postal fee item.*/
-		_CreatePostalFeeItem: function _CreatePostalFeeItem() {
-			var cartItem, postalFeeItem = this.PostalFeeItem();
-
-			if (postalFeeItem === null) {
-				return null;
-			}
-
-			postalFeeItem.CustomAttributes.push({
-				Name: 'NotRemovable',
-				Value: 'true'
-			});
-
-			cartItem = viewModelsApi.AddCartItemExtensions(this._Cart, postalFeeItem);
-
-			return cartItem;
-		},
-
-        /** Load postal fee item data generated on server. */
-		LoadPostalFeeItem: function LoadPostalFeeItem(getItemResponse) {
-			var item;
-
-			if (getItemResponse.status === 'success') {
-				item = getItemResponse.data.Item;
-
-				// Extend with some fake values that are needed in cart summary before recalc.	
-				_.extend(item, {
-					Price: item.PricingVariants[0].Price,
-					RenewalPeriod: null,
-					Category: '',
-					Discount: 0,
-					Total: item.PricingVariants[0].Price
-				});
-
-				this.PostalFeeItem(item);
-			}
-			else {
-				this.PostalFeeItem(null);
-			}
-		}
-	};
-
-
-    /** 
-     * Create pay with invoice model.
-     * @param {Object} cart - A cart view model instance.
-     * @param {Object} paymentSelection - A payment selection view model instance.
-     * @param {Object|Function} extensions - Extensions to the default pay with invoice view model.
-     */
-	CreatePayWithInvoiceModel = function CreatePayWithInvoiceModel(cart, paymentSelection, extensions) {
-		var defaults, viewModel;
-
-		defaults = {
-			_Cart: cart,
-			InvoiceType: ko.observable(),
-			PostalFeeItem: ko.observable(null)
-		};
-
-		viewModel = utils.createViewModel(PayWithInvoiceModelPrototype, defaults, extensions);
-
-		viewModel.InvoiceType.subscribe(viewModel._SelectInvoiceType, viewModel);
-		paymentSelection.SelectedPaymentMethod.subscribe(viewModel._SelectPaymentMethod, viewModel);
-
-		return viewModel;
+	        self.invoiceType('email');
+	    });
 	};
 
 
 	_.extend(exports, {
-	    CreatePayWithInvoiceModel: CreatePayWithInvoiceModel
+	    PayWithInvoiceModel: PayWithInvoiceModel
 	});
 
 })(Atomia.ViewModels, _, ko, Atomia.Utils, Atomia.ViewModels);
