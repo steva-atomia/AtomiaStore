7cdaa3c132732e4401faaf882498c622646675db
Daniel Persson <daniel.persson@atomia.com>
2015-04-29 16:55:23 +0200

    Added validation to domain search JSON method.

--- Atomia.Store.AspNetMvc/Models/DomainQueryModel.cs
+++ Atomia.Store.AspNetMvc/Models/DomainQueryModel.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using Atomia.Web.Plugin.Validation.ValidationAttributes;
+
 namespace Atomia.Store.AspNetMvc.Models
 {
     /// <summary>
@@ -9,6 +10,8 @@ namespace Atomia.Store.AspNetMvc.Models
         /// <summary>
         /// The actual domain search query string.
         /// </summary>
+        [AtomiaRequired("Common,ErrorEmptyField")]
+        [AtomiaRegularExpression("DomainSearch", "Common,ErrorInvalidDomain", true)]
         public string Query { get; set; }
     }
 }
