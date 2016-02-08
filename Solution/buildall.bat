09f516c571b8a00b81824a1fb36cc481c9c24805
Jimmy Bergman <jimmy@atomia.com>
2016-02-08 15:02:19 +0100

    Update build to work with Visual Studio 2015 and WiX 3.10

--- Solution/buildall.bat
+++ Solution/buildall.bat
@@ -1,9 +1,11 @@
+SET MSBUILD="%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe"
+
 REM Build Atomia Store
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %~dp0Store.sln /p:Configuration=Release
+%MSBUILD% %~dp0Store.sln /p:Configuration=Release
 IF %ERRORLEVEL% NEQ 0 GOTO lexit
 
 REM Build the MSI
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %~dp0AtomiaStoreInstaller.sln /p:Configuration=Release
+%MSBUILD% %~dp0AtomiaStoreInstaller.sln /p:Configuration=Release
 IF %ERRORLEVEL% NEQ 0 GOTO lexit 
 
 REM Exit test with previously recorded error status
